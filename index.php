4246eb0dad07d0951fca0cbec2f70c24f55cd53f
Dan Schneider <Dschnd@gmail.com>
2018-02-20 14:23:43 -0600

    Responsive

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>devDandy - Web Development</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 </head>
 <body>
